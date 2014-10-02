b2c7fc18561767a920e9afc78d0e94d56ef208bd
박세원 <parkse9414@naver.com>
2014-10-02 16:26:01 +0900

    dsdfs

--- sketch.js
+++ sketch.js
@@ -1,18 +1,18 @@
 function setup() 
 {
-   createCanvas(800, 800);
+   createCanvas(900, 900);
    
 }
 
 function draw()
 {
 	background(0);
-
-	if(mouseX<400){
-		
 	noFill();
     stroke(255,255,255);
 
-	ellipse(400,400,mouseX,mouseY);
+	if(mouseX<400){
+	ellipse(300,450,100,100);
+} else {
+	ellipse(600,450,100,100)
 }
 }
